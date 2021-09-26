b6162ec3018acbe34f098fc084aad7c02f6e8bf8
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2021-09-26 05:30:31 +0400

    Commit here

--- src/App.js
+++ src/App.js
@@ -10,20 +10,6 @@ import Navbar from './Navbar';
 import NotFound from './NotFound';
 
 function App() {
-  //I wrote this for Handle-Reload situations while project is uploaded on Github, when page reloading from browser-
-  //reload button, the browser sends request to wrong address which has not a correct direction of the default page, 
-  //that is why it returns response with 404 code, so i decided to check if page is reloaded, then it locates automatically
-  //to the default page of Home "/";
-  const [reloadType] = useState(performance.navigation.type);
-  const [hostName] = useState(window.location.hostname)
-
-  useEffect(() => {
-    hostName !== "localhost" ?
-      reloadType === 1 ? window.location.href = "/Donuts-Market/" : console.log("Everything Works!")
-    : console.log("Everything Works!");
-  }, [hostName, reloadType]);
-
-
   return (
     <Router>
       <div className="container">
--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,6 @@ import BlogList from "./BlogList";
 import ImageSlider from "./imageSlider/ImageSlider";
 
 const Home = () => {
-    //This Wrote for github repository, while website opens with sub-categoty /Donuts-Market/, the algorithm checks it
-    //and returns default page of Home "/";
-    const [{id}] = useState(useParams());
-    const history = useHistory();
-    useEffect(() => {
-        id === undefined ? history.push("/") : console.log("Everything Works !");
-    }, [id, history]);
-    //End Comment;
-
     return ( 
         <div>
             <ImageSlider />
